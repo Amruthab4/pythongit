69df1ad1a8aac20681053e9ed979ade43bc57cb1
siddireddy amrutha <siddireddyamrutha@gmail.com>
2022-12-06 19:38:57 +0530

    First commit

---
+++ parkinglot.py
@@ -0,0 +1,23 @@
+# noOfCars=0
+# noOfBikes=0
+# totalPayment=0
+# noOfCars=int(input("No of Cars"))
+# noOfBikes=int(input("No of Bikes"))
+# totalPayment=noOfBikes*20+noOfCars*40
+# print("total payment would be",totalPayment)
+
+# print()
+# noOfCars="Sanjoy"
+
+product1=0
+product2=0
+product3=0
+totalPayment=0
+product1=int(input("quantity of product1"))
+product2=int(input("quantity of product2"))
+product3=int(input("quantity of product3"))
+totalPayment=product1*40+product2*50+product3*60
+print("the user needs to pay",totalPayment)
+
+
+
